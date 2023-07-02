39915f8c45ccc2863e0e1a446e717006361b4f1f
Misha <shevchenkom606@gmail.com>
2023-07-02 21:11:06 +0200

    create hw

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,32 @@
+export const Profile = ({ username, avatar, tag, location, stats }) => {
+  return (
+    <div className="profile">
+        <div className="description">
+            <img
+            src={avatar}
+            alt={tag}
+            width={100}
+            className="avatar"
+            />
+                <p className="tag">@{tag}</p>
+                <p className="name">{username}</p>
+                <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+            <li>
+                <span className="label">Followers</span>
+                <span className="quantity">{stats.followers}</span>
+            </li>
+            <li>
+                <span className="label">Views</span>
+                <span className="quantity">{stats.views}</span>
+            </li>
+            <li>
+            <span className="label">Likes</span>
+            <span className="quantity">{stats.likes}</span>
+            </li>
+        </ul>
+    </div>
+  );
+};
